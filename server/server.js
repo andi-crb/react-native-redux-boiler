1e62d4f7efe4a15327a47933423797f5e3fb0d1a
andi-crb <andicbuchanan@gmail.com>
2017-04-14 13:46:46 +1200

    serving Json file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 var express = require('express')
 var app = express()
+var fs = require('fs')
+var path = require('path')
+var dataPath = path.join(__dirname, '../data/drinks.json')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/drinks', function (req, res) {
+  fs.readFile(dataPath, 'utf8', function (err, data) {
+    if (err) {
+      reject (err)
+    } else {
+      var drinks = JSON.parse(data).drinks
+      res.json('home', {drinks: drinks})
+    }
+  })
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('App listening on port 8080!')
 })
